5ff8fc21cff23ec095b686c99e85a02f1e92776a
TrentDev <TrentLathim@gmail.com>
2022-01-27 14:47:33 -0800

    Created class for champion card for later styles

---
+++ role-data/marksmen.js
@@ -0,0 +1,30 @@
+// This file holds all marksmen in javascript objects
+
+//Marksman Data for use in document
+const akshan = {
+  championName: "Akshan",
+  role: "Marksmen",
+  difficulty: "1",
+  tag: "The Rogue Sentinel",
+  description: `Raising an eyebrow in the face of danger, 
+  Akshan fights evil with dashing charisma, righteous vengeance, 
+  and a conspicuous lack of shirts. He is highly skilled in the 
+  art of stealth combat, able to evade the eyes of his enemies 
+  and reappear when they least expect him. With a keen sense of 
+  justice and a legendary death-reversing weapon, he rights the 
+  wrongs of Runeterra's many scoundrels while living by his own 
+  moral code: “Don't be an ass.”`,
+};
+
+// image: ".//images/marksmen/akshan.jpg",
+// const = {
+//     championName: "",
+//     role: "Marksmen",
+//     difficulty: "",
+//     tag: "",
+//     description: ``,
+//   };
+
+//array that holds all marksmen for use in script.js
+
+const marksmen = [akshan];
--- script.js
+++ script.js
@@ -7,15 +7,38 @@ let mageGenerated = 0;
 // Creates mage cards when mage button is clicked
 
 const generateMage = () => {
+  marksmenGenerated = 0;
   if (mageGenerated > 1) return;
   mageGenerated++;
   if (mageGenerated === 1) {
     for (let i = 0; i < mage.length; i++) {
       const championCardSelector = document.querySelector(".championCard");
       const championCardElement = document.createElement("h2");
+      championCardElement.classList.add("championCard");
       championCardElement.innerText = mage[i].championName;
       championCardSelector.append(championCardElement);
       console.log(mage[i]);
     }
   }
+};
+
+let marksmenGenerated = 0;
+
+const generateMarksmen = () => {
+  mageGenerated = 0;
+  if (marksmenGenerated > 1) return;
+  marksmenGenerated++;
+  if (marksmenGenerated === 1) {
+    for (let i = 0; i < marksmen.length; i++) {
+      const championCardSelector = document.querySelector(".championCard");
+      const championCardElement = document.createElement("h2");
+      championCardElement.innerText = marksmen[i].championName;
+      championCardSelector.append(championCardElement);
+      console.log(marksmen[i]);
+    }
+  }
+};
+
+const check = () => {
+  console.log(mageGenerated, marksmenGenerated);
 };
