8865170dbd04744530547eb60b9f4254834dffdf
TrentDev <TrentLathim@gmail.com>
2022-01-27 02:15:36 -0800

    added if check that prevents users from created more then intended objects and prevents brute force hacking

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-for (let i = 0; i < mage.length; i++) {
-  const championCardSelector = document.querySelector(".championCard");
-  const championCardElement = document.createElement("h2");
-  championCardElement.innerText = mage[i].championName;
-  championCardSelector.append(championCardElement);
-  console.log(mage[i]);
-}
+// Variable that prevents brute force hacking by
+// incrementing it onclick and returning it if it has already
+// been clicked
+
+let mageGenerated = 0;
+
+// Creates mage cards when mage button is clicked
+
+const generateMage = () => {
+  mageGenerated++;
+  if (mageGenerated > 1) return;
+  if (mageGenerated === 1) {
+    for (let i = 0; i < mage.length; i++) {
+      const championCardSelector = document.querySelector(".championCard");
+      const championCardElement = document.createElement("h2");
+      championCardElement.innerText = mage[i].championName;
+      championCardSelector.append(championCardElement);
+      console.log(mage[i]);
+    }
+  }
+};
