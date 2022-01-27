2db1fff8fb4e7f8111cc95a99c32b9d0aa7cae1e
TrentDev <TrentLathim@gmail.com>
2022-01-27 01:51:31 -0800

    for loop creates html elements generated from array of javascript object, I CANT BELIEVE THIS WORKS OMG

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < mage.length; i++) {
   const championCardSelector = document.querySelector(".championCard");
   const championCardElement = document.createElement("h2");
-  championCardElement.innerText = "Hello!";
+  championCardElement.innerText = mage[i].championName;
   championCardSelector.append(championCardElement);
   console.log(mage[i]);
 }
